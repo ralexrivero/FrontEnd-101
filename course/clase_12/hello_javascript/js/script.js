95312395874bed025c7b11cecc45e4848f15e81f
ralexrivero <ralexrivero@gmail.com>
2023-02-13 20:39:27 -0300

    flex master and js first try

---
+++ course/clase_12/hello_javascript/js/script.js
@@ -0,0 +1,3 @@
+var nombre = 'Ronald';
+
+console.log('Hola ' + nombre);
---
+++ course/clase_12/tipos_variables/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Variables del tipo primitivo
+ * No son objetos ni tienen metodos
+ * case sensitive: sensibles a las mayusculas
+ * Dinamicas: no se especifica el tipo, JavaScript lo infiere
+ */
+
+// string
+
+var texto = 'Curso de JavaScript';
+
+var TEXTO = 'Curso de HTML y CSS';
+
+var Texto = 'Otro curso'
+
+console.log(TEXTO);
+
+// Number
+
+var numero = 1;
+
+var a = 250;
+
+
+// typeof
+console.log(typeof a);

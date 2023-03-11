5a308cd92f165a3c0582fb4b9abe7354ab99140c
ralexrivero <ralexrivero@gmail.com>
2023-03-11 04:40:16 -0300

    style fix

--- clases/clase_22/lightbox/js/main.js
+++ clases/clase_22/lightbox/js/main.js
@@ -3,25 +3,23 @@ const maxiImagen = document.getElementById('maxi-img');
 const pantallaNegra = document.getElementById('maxi');
 const body = document.getElementsByTagName('body');
 
-
-for (i = 0; i < miniaturas.length; i++) {
+for (let i = 0; i < miniaturas.length; i++) {
   const fuente = miniaturas[i].src.replace(/s.jpg/g, 'm.jpg');
   miniaturas[i].addEventListener('click', () => {
     maxiImagen.src = fuente;
     mostrar();
-  })
+  });
 }
 
 function mostrar () {
   pantallaNegra.classList.remove('oculto');
-  body[0].style.overflow = "hidden";
+  body[0].style.overflow = 'hidden';
   const positionY = window.scrollY - 5;
-  pantallaNegra.style.top =`${positionY}px`
+  pantallaNegra.style.top = `${positionY}px`;
 }
 
 pantallaNegra.addEventListener('click', () => {
   pantallaNegra.classList.add('oculto');
-  body[0].style.overflow = "auto";
-  maxiImagen.src = "";
+  body[0].style.overflow = 'auto';
+  maxiImagen.src = '';
 });
-

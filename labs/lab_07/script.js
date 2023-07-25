0248d1371784d19018b418092dbac2cdfbab0149
ralexrivero <ralexrivero@gmail.com>
2023-07-24 21:43:04 -0300

    some advance in lab 07

---
+++ labs/lab_07/script.js
@@ -0,0 +1,17 @@
+const cadena = 'Hola'; // string
+const numero = 128;  // number
+const booleano = true; // boolean
+const nulo = null; // null
+let indefinido; // undefined
+
+// console.log(typeof cadena);
+
+const imprimir = `la variable cadena es de tipo ${typeof cadena}`;
+console.log(imprimir);
+
+
+const a = 16;
+let b = 18;
+
+const c = a + b;
+console.log(`${a} + ${b} = ${c}`);
